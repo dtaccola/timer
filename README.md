223a419751a898c76a31420fdac78fbdbe850c4d
dtaccola <danieltaccola@yahoo.com.br>
2022-09-28 19:17:56 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Criar um timer utilizando javascript
+[x] - Deve conter um botão para Iniciar a contagem regressiva
+[x] - Deve conter um botão para Pausar a contagem
+[x] - Deve conter um botão para Cancelar a contagem
+[x] - Deve exibir a contagem regressiva na tela
+[x] - Precisa haver um jeito de definir o tempo de contagem (ex: 10 minutos, 1 minuto, 1 hora, 59 segundos, etc)
+
+BONUS
+[] - Ao final tocar um alarme
+[X] - Ao pressionar a tecla "I" deve iniciar o timer, se precionar a tecla "P" deve pausar.
+[X] - O botão pausar e iniciar devem ser o mesmo botão, ou seja, quando estiver contando fica "Pausar" quando estiver parado fica "Iniciar", alterando apenas o texto do botão
---
+++ main.js
@@ -0,0 +1,70 @@
+var seconds = document.querySelector('#seconds').value;
+var minutes = document.querySelector('#minutes').value;
+var hours = document.querySelector('#hours').value;
+var alarm = document.querySelector('#sound'); 
+var interval;
+
+var start_btn = document.querySelector('.start-btn');
+var pause_btn = document.querySelector('.pause-btn');
+
+document.body.addEventListener('keypress', function (event) {
+  const key = event.key;
+  const code = event.keyCode;
+  console.log(`Key: ${key}, Code ${code}`);
+
+  if (key == 'i' || key == 'I') {
+    start();
+  }else{
+    key == 'p' || key == 'P'
+    pause();
+  }
+
+});
+
+function twoDigits(digit) {
+  if(digit < 10){
+    return('0' + digit);
+  }else{
+    return(digit);
+  }
+}
+
+function start() {
+  watch();
+  // interval = setInterval(watch, 1000);
+  interval = setInterval(watch, 100);
+  start_btn.setAttribute('style', 'display: none');
+  pause_btn.removeAttribute('style', 'display: none');
+}
+
+function pause() {
+  pause_btn.setAttribute('style', 'display: none');
+  start_btn.removeAttribute('style', 'display: none');
+  clearInterval(interval);
+}
+
+function stop() {
+  clearInterval(interval);
+  document.getElementById('watch').innerText = '00:00:00';
+  pause_btn.setAttribute('style', 'display: none');
+  start_btn.removeAttribute('style', 'display: none');
+  setInterval(location.reload(), 2000);
+}
+
+function watch() {
+  seconds--;
+  if(seconds <= 0){
+    if(minutes > 0){
+      minutes--;
+      seconds = 59;
+    }else if(minutes <= 0) {
+      hours--;
+      minutes = 59;
+    }else{ 
+      stop();
+    }
+  }
+
+  document.getElementById('watch').innerText = twoDigits(hours) + ':' + twoDigits(minutes) + ':' + twoDigits(seconds);
+
+}
